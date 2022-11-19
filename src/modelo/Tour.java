1e3188a1201f0519123df8ed7cd470702df93785
User <User@User-PC>
2022-11-19 00:16:58 -0500

    setter and getter de Tour

--- src/modelo/Tour.java
+++ src/modelo/Tour.java
@@ -15,10 +15,45 @@ public class Tour {
     private float precioTour;
     private int horas;
 
+    public Tour() {
+    }
+
     public Tour(String nombrePaquete, String itinerario, float precioTour, int horas) {
         this.nombrePaquete = nombrePaquete;
         this.itinerario = itinerario;
         this.precioTour = precioTour;
+        this.horas = horas;
+    }
+
+    public String getNombrePaquete() {
+        return nombrePaquete;
+    }
+
+    public void setNombrePaquete(String nombrePaquete) {
+        this.nombrePaquete = nombrePaquete;
+    }
+
+    public String getItinerario() {
+        return itinerario;
+    }
+
+    public void setItinerario(String itinerario) {
+        this.itinerario = itinerario;
+    }
+
+    public float getPrecioTour() {
+        return precioTour;
+    }
+
+    public void setPrecioTour(float precioTour) {
+        this.precioTour = precioTour;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
         this.horas = horas;
     }
     
