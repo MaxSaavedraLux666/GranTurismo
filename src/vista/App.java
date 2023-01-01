42689085a596d9a5a1666d0a55576d863b082cfc
User <User@User-PC>
2023-01-01 11:40:12 -0500

    Serialización y Deserialización de GuiaArreglo correctos

--- src/vista/App.java
+++ src/vista/App.java
@@ -17,13 +17,12 @@ public class App implements Serializable{
 
     public static void main(String[] args) {
 
-        Tour tour1;
         Itinerario itinerario;
         Cliente cliente;
 
         //Tours
         TourArreglo tours = new TourArreglo(1); //Correcto
-        ItinerarioArreglo itinerarios;
+        
         //Personas
         PersonaArreglo personas = new PersonaArreglo(1); //Correcto
         //Guia
@@ -33,41 +32,6 @@ public class App implements Serializable{
         VehiculoArreglo vehiculos = new VehiculoArreglo(1); //Correcto
         Vehiculo vehiculo;
 
-        /*Tours Llenado
-        itinerarios = new ItinerarioArreglo(1);
-        itinerario = new Itinerario("c34", "Recojo del transporte", " 4:45 ");
-        itinerarios.agregarItinerario(itinerario);
-        itinerario = new Itinerario("d34", "Llegada al lugar", " 5:15 ");
-        itinerarios.agregarItinerario(itinerario);
-        itinerario = new Itinerario("e34", "Ingreso a Machu Picchu", " 5:25 ");
-        itinerarios.agregarItinerario(itinerario);
-        tour1 = new Tour("Machu Picchu", itinerarios, 14.5f, 8, "CD23");
-        tours.agregarTour(tour1);
-        System.out.println("------------------------------------------------------------------------------------------------------------");
-
-        itinerarios = new ItinerarioArreglo(1);
-        itinerario = new Itinerario("f34", "Desayunno", " 6:45 ");
-        itinerarios.agregarItinerario(itinerario);
-        itinerario = new Itinerario("g34", "Almuerzo", " 12:15 ");
-        itinerarios.agregarItinerario(itinerario);
-        itinerario = new Itinerario("h34", "Cena", " 7:25 ");
-        itinerarios.agregarItinerario(itinerario);
-        tour1 = new Tour("Cusco historico", itinerarios, 14.5f, 7, "CF23");
-        tours.agregarTour(tour1);
-        System.out.println("------------------------------------------------------------------------------------------------------------");
-
-        itinerarios = new ItinerarioArreglo(1);
-        itinerario = new Itinerario("t23", " Paseo en bus por la ciudadela", " 11:20 ");
-        itinerarios.agregarItinerario(itinerario);
-        itinerario = new Itinerario("y23 ", "Ceremonia del Inti Raymi", "12:20 ");
-        itinerarios.agregarItinerario(itinerario);
-        itinerario = new Itinerario("u23", "Ceremonia del Inti Raymi", "12:20 ");
-        itinerarios.agregarItinerario(itinerario);
-        tour1 = new Tour("Iglesia", itinerarios, 14.5f, 6, "CU23");
-        tours.agregarTour(tour1);
-        System.out.println("------------------------------------------------------------------------------------------------------------");
-
-        /////////////////////*/
         //cliente = new Cliente("123456", "Sebastian Landeo", "sebastian.landeo@unmsm.edu.pe", "959637429", "72345678", 21);
         //personas.agregar(cliente);
 
@@ -95,6 +59,7 @@ public class App implements Serializable{
         
         tours = (TourArreglo) SerializadoraGen.deserializar("tours.txt");
         //personas = (PersonaArreglo) SerializadoraGen.deserializar("personas.txt");
+        guias = (GuiaArreglo) SerializadoraGen.deserializar("guias.txt");
         //Salidas
         fmrPrincipal vista = new fmrPrincipal();
         ControladorPrincipal controlador = new ControladorPrincipal(vista, tours,
